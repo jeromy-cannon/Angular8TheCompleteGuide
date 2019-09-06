99757e78db2975e8188a2ab61dfcb80e9fccd9d3
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-09-06 17:07:11 -0500

    finished section 2 assignment 1

--- section01/my-dream-app/src/app/app.module.ts
+++ section01/my-dream-app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessMessageComponent,
+    WarningMessageComponent
   ],
   imports: [
     BrowserModule ,
