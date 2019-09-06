3bdcc31042e9b8f619bc3ad6f1c8d65a239d51c9
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-09-06 17:51:09 -0500

    saving progress on section 2

--- section01/my-dream-app/src/app/servers/servers.component.ts
+++ section01/my-dream-app/src/app/servers/servers.component.ts
@@ -4,17 +4,32 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '.app-servers',
   // selector: '[app-servers]',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server>
-    </app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server>
+  //   </app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
