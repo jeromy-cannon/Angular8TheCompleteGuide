c8c6669a435a5b92521338c3fe1393c3a468f826
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-09-09 18:09:48 -0500

    working on section3

--- FillMyFridge/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FillMyFridge/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // https://elavegan.com/gluten-free-vegan-gnocchi-recipe/
+  recipes: Recipe[] = [
+    new Recipe(
+      'Gluten-Free Vegan Gnocchi',
+      'Easy Homemade Recipe',
+      'https://elavegan.com/wp-content/uploads/2019/01/Homemade-gluten-free-vegan-gnocchi-with-roasted-chickpeas-spinach-and-cheese-sauce-in-a-black-pan.jpg')
+  ];
 
   constructor() { }
 
--- FillMyFridge/src/app/shopping-list/shopping-list.component.ts
+++ FillMyFridge/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('potatoes, peeled and chopped (lbs)', 2.25),
+    new Ingredient('chickpea flour (1.5-2 cups)', 2),
+    new Ingredient('tapioca flour (cup)', 1),
+    new Ingredient('sea salt (tsp)', 0.5)
+  ];
 
   constructor() { }
 
